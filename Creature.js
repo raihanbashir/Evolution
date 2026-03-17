b3be07923931e5a2b4e9e83f299a3ebb6da2aff2
raihanbashir <you@example.com>
2026-03-17 15:23:26 +0400

    Changed how the DNA and the creature vectors work

--- Creature.js
+++ Creature.js
@@ -1,24 +1,54 @@
 class Creature {
     constructor(dna) {
         this.dna = dna;
-        this.pos = createVector(width / 2, height / 2);
+        this.pos = createVector(width / 2, height - 20); // Start at bottom
+        this.vel = createVector();
+        this.acc = createVector();
+        this.geneCounter = 0; // Which "instruction" are we on?
         this.fitness = 0;
-      
-      // Map DNA (0-1) to actual physical traits
-        this.size = map(this.dna.genes[0], 0, 1, 10, 50);
-        this.speed = map(this.dna.genes[1], 0, 1, 2, 10);
     }
-  
+    
+    applyForce(force) {
+        // Add the force to the acceleration
+        this.acc.add(force);
+    }
+
     update() {
-      // Simple logic: move right
-        this.pos.x += this.speed;
+        // 1. The Brain Logic: Apply the current gene as a force
+        this.applyForce(this.dna.genes[this.geneCounter]);
+        // Increment gene counter
+        this.geneCounter++;
+
+        // 2. Physics Engine: standard Euler integration
+        // Update velocity with acceleration
+        this.vel.add(this.acc);
+        // Update position with velocity
+        this.pos.add(this.vel);
+        // Reset acceleration to 0
+        this.acc.mult(0);
     }
   
     show() {
-        fill(255, 100);
-        stroke(255);
-        ellipse(this.pos.x, this.pos.y, this.size);
-    }
+        noStroke();
+        
+        // Use the magnitude of velocity to make them "stretch"
+        let speed = this.vel.mag();
+        let stretch = map(speed, 0, 5, 0, 15);
+        
+        push();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.vel.heading());
+        
+        // Outer soft glow
+        fill(100, 200, 255, 50); 
+        ellipse(0, 0, 30 + stretch, 20);
+        
+        // Inner core
+        fill(255, 200); 
+        ellipse(0, 0, 15 + stretch, 10);
+        
+        pop();
+      }
 
     calcFitness() {
         let d = dist(this.pos.x, this.pos.y, target.x, target.y);
@@ -29,6 +59,11 @@ class Creature {
         
         // 2. Apply the exponent
         // The higher the power, the more "picky" the evolution becomes
-        this.fitness = pow(score, 4); 
+        this.fitness = pow(score, 4);
+
+        // CRITICAL : Bonus for actually hitting the target
+        if (d < 10){
+            this.fitness *= 10;
+        }
     }
 }
--- DNA.js
+++ DNA.js
@@ -1,30 +1,36 @@
 class DNA {
     constructor(length) {
-      this.genes = [];
-      // Initialize with random values between 0 and 1
-      for (let i = 0; i < length; i++) {
-        this.genes[i] = random(0, 1);
-      }
+        this.genes = [];
+        // Initialize with random values between 0 and 1 using p5.Vector.random2D()
+        for (let i = 0; i < length; i++) {
+            this.genes[i] = p5.Vector.random2D();
+            // Set the magnitude of the gene to 0.2
+            this.genes[i].setMag(0.2);
+        }
     }
   
     // Combine two parents to create a child
     crossover(partner) {
-      let childDNA = new DNA(this.genes.length);
-      let midpoint = floor(random(this.genes.length));
-      
-      for (let i = 0; i < this.genes.length; i++) {
-        if (i > midpoint) childDNA.genes[i] = this.genes[i];
-        else childDNA.genes[i] = partner.genes[i];
-      }
-      return childDNA;
+        let childDNA = new DNA(this.genes.length);
+        // Choose a random midpoint to crossover
+        let midpoint = floor(random(this.genes.length));
+        // If the gene is after the midpoint, inherit from the first parent
+        for (let i = 0; i < this.genes.length; i++) {
+            if (i > midpoint) childDNA.genes[i] = this.genes[i];
+            // If the gene is before the midpoint, inherit from the second parent
+            else childDNA.genes[i] = partner.genes[i];
+        }
+        // Return the child DNA
+        return childDNA;
     }
   
     // Randomly nudge genes to allow for "evolution"
     mutate(rate) {
-      for (let i = 0; i < this.genes.length; i++) {
-        if (random(1) < rate) {
-          this.genes[i] = random(0, 1);
+        for (let i = 0; i < this.genes.length; i++) {
+            // If the random number is less than the mutation rate, mutate the gene
+            if (random(1) < rate) {
+                this.genes[i] = random(0, 1);
+            }
         }
-      }
     }
-  }
+}
