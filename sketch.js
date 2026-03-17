6094616054c25c338d97c23e2f6d82104812acea
raihanbashir <you@example.com>
2026-03-17 15:40:16 +0400

    Added reproducion and mating logic

--- sketch.js
+++ sketch.js
@@ -8,21 +8,49 @@ function setup() {
     createCanvas(800, 400);
     target = createVector(width - 50, height / 2);
     for (let i = 0; i < popSize; i++) {
-        let dna = new DNA(2); // 2 genes: size and speed
+        let dna = new DNA(lifetime); // Each DNA needs 200 vectors (one for each frame of life)
         population[i] = new Creature(dna);
     }
 }
 
 function draw() {
     background(30);
+    // Show the goal
     fill(0, 255, 0);
-    noStroke();
     ellipse(target.x, target.y, 24, 24);
 
-    for (let c of population) {
-        c.update();
-        c.show();
+    if (counter < lifetime) {
+        // WHILE ALIVE: Move and draw
+        for (let i = 0; i < population.length; i++) {
+            population[i].update();
+            population[i].show();
+        }
+        counter++;
+    } else {
+        // WHEN DEAD: Evolve
+        let matingPool = evaluate(); // Build the pool of winners
+        reproduction(matingPool);    // Create the next generation
+        counter = 0;                 // Reset the clock for Generation X+1
     }
+}
+
+function reproduction(matingPool) {
+    let nextGeneration = [];
+    // 1. Pick two random parents from the pool
+    // Because better creatures are in the pool more often, 
+    // they have a higher chance of being picked.
+    for (let i = 0; i < popSize; i++) {
+        let parentA = random(matingPool);
+        let parentB = random(matingPool);
+        // 2. Create a child DNA by crossing over the parents
+        let childDNA = parentA.dna.crossover(parentB.dna);
+        // 3. Mutate (add random noise to prevent "stagnation")
+        childDNA.mutate(0.01); // 1% mutation rate
+        // 4. Fill the new generation array
+        nextGeneration[i] = new Creature(childDNA);
+    }
+    // 5. Replace the old population with the new one
+    population = nextGeneration;
 }
 
 function evaluate() {
@@ -36,12 +64,12 @@ function evaluate() {
     // Add to pool based on fitness score
     for (let i = 0; i < population.length; i++) {
       // Multiply fitness to get a whole number of "tickets"
-        let n = floor(population[i].fitness * 100); 
+        let n = floor(population[i].fitness * 100) + 1; 
+        // Add the creature to the mating pool based on its fitness score
         for (let j = 0; j < n; j++) {
-            // Add the creature to the mating pool based on its fitness score
             matingPool.push(population[i]);
         }
-        // Return the mating pool
     }
+    // Return the mating pool
     return matingPool;
 }
